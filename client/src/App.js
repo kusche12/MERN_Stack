bee8f5847f91d03348359bdc96d11619cc1ff17b
kyle <funnyguykyo@gmail.com>
2020-07-23 16:52:46 -0700

    Allow user to create profile

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
-
+import CreateProfile from './components/profile-form/CreateProfile';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import './App.css';
@@ -36,6 +36,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
