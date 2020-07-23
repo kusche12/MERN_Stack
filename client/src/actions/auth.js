81f9dc9d4256ef3e3c695135cf0111d3947dadc5
kyle <funnyguykyo@gmail.com>
2020-07-22 17:58:28 -0700

    Client side react router and redux for authentication

---
+++ client/src/actions/auth.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  AUTH_ERROR,
+  USER_LOADED,
+} from './types';
+import setAuthToken from '../utils/setAuthToken';
+
+// Load user
+export const loadUser = () => async (dispatch) => {
+  // If a token is saved, then put it in the global header
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
+
+// Register user
+export const register = ({ name, email, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const body = JSON.stringify({ name, email, password });
+  try {
+    const res = await axios.post('api/users', body, config);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((err) => dispatch(setAlert(err.msg, 'danger')));
+    }
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
